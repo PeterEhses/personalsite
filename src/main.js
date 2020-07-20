d008785b59dc8bcbc66ba94a35ee8605eb75ccf9
PeterEhses <ehsesp@hochschule-trier.de>
2020-07-20 19:50:36 +0200

    -

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" >
+    <div class="" v-for="idx in numSegments" :key="idx">
+    </div>
+      <img :src="'https://picsum.photos/seed/' + this.idx + '/1024'" alt="">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import ColorThief from 'colorthief/dist/color-thief.mjs'
+// const colorThief = new ColorThief();
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
+  },
+    data: function(){
+    return {
+      numSegments: 4,
+    }
+  }, computed: {
+    // backgrounds: function(){
+    //   let bgcol = [];
+    //   for(let i = 0; i < this.numSegments; i++){
+    //
+    //     let theCol = Math.random()*360;
+    //     gbcol.append(theCol)
+    //   }
+    //   return bgcol;
+    // }
+  },
+  mounted: function() {
+    let that = this;
+    this.axios.get("https://picsum.photos/v2/list").then((response) => {
+      console.log(response.data)
+      that.picdata = response.data;
+} );
+}
 }
 </script>
