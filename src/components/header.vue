4a1d73995be784f01ae0a287bb744aa96b900a41
PeterEhses <ehsesp@hochschule-trier.de>
2020-12-18 23:20:17 +0100

    first level fancy burger menu

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header id="nav">
+  <header id="nav" :class="[this.hamburgerActive ? 'is-active' : null]">
     <router-link to="/">
       <div id="logo">
         <h2>Peter Ehses</h2>
@@ -7,12 +7,19 @@
         <h1>Portfolio</h1>
       </div>
     </router-link>
+    <button :class= '["hamburger", "hamburger--collapse", this.hamburgerActive ? "is-active" : null]' type="button" @click="toggleMenu" aria-label="Menu" aria-controls="navigation">
+    <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+    <div class="header-blur">
+
+    </div>
     <nav>
       <router-link to="/about" class="nav" title="Kontakt">About</router-link>
       <hr />
       <router-link to="/projects" class="nav" title="Projekte">Projekte</router-link>
       <!-- <hr />
-      BREAK AT 1300px
       <router-link to="/about" class="nav" title="Blog">Blog</router-link> -->
 
     </nav>
@@ -23,8 +30,85 @@
 
 <script>
 export default {
+  data(){
+    return {
+      hamburgerActive: false,
+    }
+  },
+  methods: {
+    toggleMenu(){
+      this.hamburgerActive = !this.hamburgerActive
+    }
+  }
+
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+.hamburger, .header-blur{
+  visibility: hidden;
+}
+
+@media  (max-width: 1300px){
+  .hamburger{
+    visibility: visible;
+  }
+
+  $hamburger-types     : (collapse);
+  $hamburger-padding-x           : 2rem !default;
+  $hamburger-padding-y           : 0rem !default;
+  $hamburger-layer-width         : 2rem !default;
+  $hamburger-layer-height        : .15rem !default;
+  $hamburger-layer-spacing       : .51rem !default;
+  $hamburger-layer-color         : var(--content-color) !default;
+  $hamburger-layer-border-radius : 0em !default;
+  @import "../scss/hamburgers/hamburgers.scss";
+  #nav{
+    .header-blur{ // this makes the background pretty on supported and prevents some scroll on all
+      z-index: -1;
+      visibility: visible;
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100vw;
+      height: 100vh;
+      backdrop-filter: blur(10px);
+      transform: translate(100%, 0);
+      transition: all calc(var(--transition-time) / 2);
+    }
+    &.is-active{
+      .hamburger{
+        position: fixed;
+        right: 1rem;
+      }
+      .header-blur{
+        transform: translate(0%, 0);
+      }
+      nav{
+        transform: translate(0%, 0);
+      }
+    }
+
+    nav{
+      transform: translate(100%, 0);
+      transition: all calc(var(--transition-time) / 2);
+      background-color: var(--bg);
+      z-index: -1;
+      position: fixed;
+      right: 0;
+      top: 0;
+      height: 100vh;
+      border-left: var(--border-width) solid var(--content-color);
+      padding: 3rem;
+      padding-top: 7rem;
+      text-align: right;
+      hr{
+        display: block;
+        width: 0;
+        opacity: 0;
+      }
+    }
+  }
+
+}
 </style>
